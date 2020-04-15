41280455116e9db973d8648168641dc25ecf57dd
Uwe Sauerbrei <info@uwe-sauerbrei.de>
2020-04-15 19:16:55 +0200

    Initial commit

---
+++ src/main/java/de/ostfale/sbsia/SbsiaApplication.java
@@ -0,0 +1,13 @@
+package de.ostfale.sbsia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbsiaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SbsiaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/de/ostfale/sbsia/SbsiaApplicationTests.java
@@ -0,0 +1,13 @@
+package de.ostfale.sbsia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbsiaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
